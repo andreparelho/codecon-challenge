a940ed03a7be6e590bd65fce5c62b23af5463283
Andre Parelho <parelhoneves.andre@gmail.com>
2025-04-24 20:41:52 -0300

    feat: fix handler

--- internal/handler/evaluation_handler.go
+++ internal/handler/evaluation_handler.go
@@ -4,5 +4,9 @@ import "net/http"
 
 func Evaluation() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.WriteHeader(http.StatusOK)
 	}
 }
--- internal/handler/user_handler.go
+++ internal/handler/user_handler.go
@@ -4,20 +4,36 @@ import "net/http"
 
 func SaveUsers() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
 func GetSuperUsers() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
 func GetSuperUsersByTopCountries() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.WriteHeader(http.StatusOK)
 	}
 }
 
 func GetActiveUsers() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusBadRequest)
+		}
+		w.WriteHeader(http.StatusOK)
 	}
 }
