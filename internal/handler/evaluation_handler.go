14b6ee4b6c0f78e7467acd49b49e933cb39c3496
Andre Parelho <parelhoneves.andre@gmail.com>
2025-04-24 21:29:51 -0300

    feat: fix return

--- internal/handler/evaluation_handler.go
+++ internal/handler/evaluation_handler.go
@@ -6,6 +6,7 @@ func Evaluation() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
 		w.WriteHeader(http.StatusOK)
 	}
