13aef84efd9789763bcbd01d4a9be0d1390fe335
Andre Parelho <parelhoneves.andre@gmail.com>
2025-04-27 13:21:09 -0300

    feat: team logs

--- internal/handler/team_handler.go
+++ internal/handler/team_handler.go
@@ -3,22 +3,36 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/andreparelho/codecon-challenge/internal/repository"
+	"github.com/sirupsen/logrus"
 )
 
 func GetMembers(u repository.UserRepository) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer r.Body.Close()
+		start := time.Now()
 
 		if r.Method != http.MethodGet {
-			w.WriteHeader(http.StatusBadRequest)
+			logrus.WithFields(logrus.Fields{
+				"method": r.Method,
+				"url":    r.URL,
+			}).Error("this method not supported")
+
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
 
 		teams, err := u.GetMembers()
 		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"method": r.Method,
+				"url":    r.URL,
+			}).Error("error to get teams")
 
+			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
 		userHandlerResponse := UserHandlerResponse{
@@ -30,9 +44,19 @@ func GetMembers(u repository.UserRepository) http.HandlerFunc {
 
 		var response []byte
 		if response, err = json.Marshal(userHandlerResponse); err != nil {
+			logrus.WithFields(logrus.Fields{
+				"method": r.Method,
+				"url":    r.URL,
+			}).Error("error to unmarshal teams")
+
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		logrus.WithFields(logrus.Fields{
+			"timestamp": time.Since(start),
+			"teams":     len(teams),
+		}).Info("success to get active users")
 
 		w.WriteHeader(http.StatusOK)
 		w.Write(response)
