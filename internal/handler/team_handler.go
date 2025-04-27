c2e844384926ff30489d4c5bed8fee7fcc9ab5db
Andre Parelho <parelhoneves.andre@gmail.com>
2025-04-27 01:25:25 -0300

    feat: fix routes and team handler

--- internal/handler/team_handler.go
+++ internal/handler/team_handler.go
@@ -4,10 +4,13 @@ import "net/http"
 
 func GetMembers() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
 		if r.Method != http.MethodGet {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+
 		w.WriteHeader(http.StatusOK)
 	}
 }
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -11,7 +11,7 @@ func RegisterRoutes(u repository.UserRepository) {
 	http.HandleFunc("/user", handler.SendUsersFile(u))
 	http.HandleFunc("/superusers", handler.GetSuperUsers(u))
 	http.HandleFunc("/top-countries", handler.GetTopCountries(u))
-	http.HandleFunc("/active-users-per-day", handler.GetActiveUsers())
+	http.HandleFunc("/active-users-per-day", handler.GetActiveUsers(u))
 	http.HandleFunc("/team-insights", handler.GetMembers())
 	http.HandleFunc("/evaluation", handler.Evaluation())
 }
